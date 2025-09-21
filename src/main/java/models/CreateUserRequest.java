685c108a0bb5aa0125dab18746e36af62db2a1ec
artyom <artyomredmi94@gmail.com>
2025-09-21 18:17:38 +0400

    added auto generation for CreateUserRequest model

--- src/main/java/models/CreateUserRequest.java
+++ src/main/java/models/CreateUserRequest.java
@@ -1,12 +1,20 @@
 package models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import utils.annotations.GeneratePattern;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequest extends BaseModel {
+    @GeneratePattern(regex = "[a-zA-Z0-9._-]{3,15}")
     private String username;
+    @GeneratePattern(regex = "[A-Z][0-9][!@#$%^&=+][a-z]{5,27}")
     private String password;
+    @GeneratePattern(regex = "USER")
     private String role;
 }
--- src/main/java/utils/HttpLoggingFilter.java
+++ src/main/java/utils/HttpLoggingFilter.java
@@ -2,6 +2,7 @@ package utils;
 
 import io.restassured.filter.Filter;
 import io.restassured.filter.FilterContext;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
@@ -10,6 +11,18 @@ public class HttpLoggingFilter implements Filter {
     @Override
     public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext context) {
         System.out.println("➡\uFE0F [REQUEST] " + requestSpec.getMethod() + " " + requestSpec.getURI());
+
+        String requestBody = requestSpec.getBody();
+        if (requestBody != null && !requestBody.isEmpty()) {
+            try {
+                // выводим JSON в отформатированном виде
+                String prettyBody = JsonPath.from(requestBody).prettify();
+                System.out.println(prettyBody);
+            } catch (Exception e) {
+                // если тело не является валидным JSON, выводим как есть
+                System.out.println(requestBody);
+            }
+        }
 
         Response response = context.next(requestSpec, responseSpec);
 
