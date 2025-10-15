5b9f323177dce9413eb31f71169435e08579534e
artyom <artyomredmi94@gmail.com>
2025-10-15 22:17:01 +0400

    added tests in DepositMoneyTest

--- src/test/java/iteration2/ui/BaseTest.java
+++ src/test/java/iteration2/ui/BaseTest.java
@@ -1,15 +1,26 @@
 package iteration2.ui;
 
 import com.codeborne.selenide.Configuration;
+import configs.Config;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.util.Map;
 
 public class BaseTest {
+    protected static final float MAX_DEPOSIT_AMOUNT = 5000.00f;
+    protected static final float MAX_TRANSFER_AMOUNT = 10000.00f;
+
+    @BeforeAll
+    public static void setupRestAssured() {
+        RestAssured.baseURI = Config.getString("service.host");
+        RestAssured.port = Config.getInt("service.port");
+    }
+
     @BeforeAll
     public static void setupSelenoid() {
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.baseUrl = "http://192.168.70.184:3000";
+        Configuration.remote = Config.getString("selenoid.url");
+        Configuration.baseUrl = Config.getString("service.host");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
 
--- src/test/java/iteration2/ui/DepositMoneyTest.java
+++ src/test/java/iteration2/ui/DepositMoneyTest.java
@@ -1,13 +1,17 @@
 package iteration2.ui;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import generators.RandomData;
+import models.BankAccount;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoSuchElementException;
 import steps.User;
+import utils.TestUtils;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DepositMoneyTest extends BaseTest {
     private static User user;
@@ -19,8 +23,15 @@ public class DepositMoneyTest extends BaseTest {
                 .createRandomUser()
                 .createFirstAccount()
                 .build();
-//        new ValidationRequester(Endpoint.LOGIN, RequestSpecs.authAsAdmin(), ResponseSpecs.returnsOk())
-//                .send(new LoginRequest("admin", "admin"));
+    }
+
+    @BeforeEach
+    public void openDashboardPageAsUser() {
+        // Открываем браузер и сохраняем токен в local storage
+        Selenide.open("/");
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", user.token());
+
+        Selenide.open("/dashboard");
     }
 
     // Удаляем юзера после прохождения всех тестов
@@ -32,8 +43,95 @@ public class DepositMoneyTest extends BaseTest {
     @Test
     @DisplayName("Успешное пополнение баланса")
     public void userCanDepositTest() {
-        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", user.token());
+        // Получаем данные счета
+        BankAccount account = user.getProfile()
+                .getAccounts()
+                .stream()
+                .filter((acc) -> acc.getId() == user.firstAccountId())
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Счет с id: " + user.firstAccountId() + " не найден!"));
 
-        Selenide.open("/dashboard");
+        String accountNumber = account.getAccountNumber();
+        float initialBalance = account.getBalance();
+
+        $$("button").findBy(Condition.text("Deposit Money")).click();
+
+        // Проверям, что открыта страница Deposit Money
+        $(".deposit-input").shouldBe(Condition.visible);
+
+        $(".account-selector").selectOptionByValue(String.valueOf(user.firstAccountId()));
+
+        float amount = RandomData.getAmount(MAX_DEPOSIT_AMOUNT);
+        $(".deposit-input").setValue(String.valueOf(amount));
+
+        $(".btn-primary").click();
+
+        Alert alert = switchTo().alert();
+        String expectedMessage = String.format("Successfully deposited $%s to account %s!", amount, accountNumber);
+        assertThat(alert.getText()).contains(expectedMessage);
+        alert.accept();
+
+        // Проверяем, что выполнен переход на страницу User Dashboard
+        $(".welcome-text").shouldBe(Condition.visible);
+
+        // Проверяем, что баланс пользователя изменился
+        float expectedBalance = TestUtils.getCorrectAmount(initialBalance + amount);
+        assertThat(user.getFirstAccountBalance()).isEqualTo(expectedBalance);
+    }
+
+    @Test
+    @DisplayName("Пополнение баланса на сумму, превышающую максимальную")
+    public void userCannotDepositGreaterThanAllowedAmountTest() {
+        // Получаем текущий баланс пользователя
+        float initialBalance = user.getFirstAccountBalance();
+
+        $$("button").findBy(Condition.text("Deposit Money")).click();
+
+        // Проверям, что открыта страница Deposit Money
+        $(".deposit-input").shouldBe(Condition.visible);
+
+        $(".account-selector").selectOptionByValue(String.valueOf(user.firstAccountId()));
+
+        $(".deposit-input").setValue(String.valueOf(MAX_DEPOSIT_AMOUNT + 0.01f));
+
+        $(".btn-primary").click();
+
+        Alert alert = switchTo().alert();
+        assertThat(alert.getText()).contains("Please deposit less or equal to 5000$");
+        alert.accept();
+
+        // Проверям, что открыта страница Deposit Money
+        $(".deposit-input").shouldBe(Condition.visible);
+
+        // Проверяем, что баланс пользователя не изменился
+        assertThat(user.getFirstAccountBalance()).isEqualTo(initialBalance);
+    }
+
+    @Test
+    @DisplayName("Невалидная сумма при пополнении баланса")
+    public void userCannotDepositWithInvalidAmountTest() {
+        // Получаем текущий баланс пользователя
+        float initialBalance = user.getFirstAccountBalance();
+
+        $$("button").findBy(Condition.text("Deposit Money")).click();
+
+        // Проверям, что открыта страница Deposit Money
+        $(".deposit-input").shouldBe(Condition.visible);
+
+        $(".account-selector").selectOptionByValue(String.valueOf(user.firstAccountId()));
+
+        $(".deposit-input").setValue("0");
+
+        $(".btn-primary").click();
+
+        Alert alert = switchTo().alert();
+        assertThat(alert.getText()).contains("Please enter a valid amount");
+        alert.accept();
+
+        // Проверям, что открыта страница Deposit Money
+        $(".deposit-input").shouldBe(Condition.visible);
+
+        // Проверяем, что баланс пользователя не изменился
+        assertThat(user.getFirstAccountBalance()).isEqualTo(initialBalance);
     }
 }
