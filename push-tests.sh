50912aad8e4bae69c84d189b31a0ab297ba33eb6
artyom <artyomredmi94@gmail.com>
2025-11-25 22:08:02 +0400

    refactor push-tests.sh for build multi-arch image

--- push-tests.sh
+++ push-tests.sh
@@ -8,8 +8,18 @@ DOCKERHUB_TOKEN=token
 echo ">>> Авторизация в Docker Hub с токеном"
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 
+# Создаём buildx builder (или используем существующий)
+docker buildx create --use --name multiarch-builder >/dev/null 2>&1 || docker buildx use multiarch-builder
+
+echo ">>> Сборка multi-arch образа (amd64 + arm64)"
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t $IMAGE_NAME:$TAG \
+  --load \
+  .
+
 echo ">>> Тегирование образа"
-docker tag $IMAGE_NAME $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
+docker tag $IMAGE_NAME:$TAG $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
 
 echo ">>> Отправка образа в Docker Hub"
 docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
