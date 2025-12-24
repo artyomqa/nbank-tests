99f98e156b5efb8f606b88c7ff01a4dce81adc2d
artyom <artyomredmi94@gmail.com>
2025-12-25 00:14:32 +0400

    added grafana and kibana to cluster

--- kube/start-kube.sh
+++ kube/start-kube.sh
@@ -3,10 +3,10 @@
 echo ">>> Запуск minikube"
 minikube start --driver=docker
 
-kubectl create configmap selenoid-config --from-file=browsers.json=./kube/nbank-chart/files/browsers.json
+kubectl create configmap selenoid-config --from-file=browsers.json=./nbank-chart/files/browsers.json
 
 echo ">>> Установка сервисов в Kubernetes-кластер"
-helm install nbank ./kube/nbank-chart
+helm install nbank ./nbank-chart
 
 echo ">>> Ожидание готовности подов"
 kubectl wait --for=condition=available --timeout=300s deployment/frontend
@@ -19,5 +19,20 @@ kubectl port-forward svc/frontend 3000:80 > /dev/null 2>&1 &
 kubectl port-forward svc/backend 4111:4111 > /dev/null 2>&1 &
 kubectl port-forward svc/selenoid 4444:4444 > /dev/null 2>&1 &
 kubectl port-forward svc/selenoid-ui 8080:8080 > /dev/null 2>&1 &
+
+echo ">>> Установка Prometheus, Grafana"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
+helm repo add elastic https://helm.elastic.co || true
+helm repo update
+
+helm upgrade --install monitoring prometheus-community/kube-prometheus-stack -n monitoring --create-namespace -f monitoring-values.yaml
+
+echo  ">>> Проброс портов для Prometheus и Grafana"
+kubectl port-forward svc/monitoring-kube-prometheus-prometheus -n monitoring 3001:9090 > /dev/null 2>&1 &
+kubectl port-forward svc/monitoring-grafana -n monitoring 3002:80 > /dev/null 2>&1 &
+
+echo ">>> Создание секретов для авторизации на бэкенде"
+kubectl create secret generic backend-basic-auth --from-literal=username=admin --from-literal=password=admin -n monitoring
+kubectl apply -f spring-monitoring.yaml
 
 echo ">>> Выполнено"
